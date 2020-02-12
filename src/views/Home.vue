e3f10a58208a8d91409e2ff5d57c1e916bd216b3
Cesar Oliveira dos Santos Araujo <caraujo@jc.com.br>
2020-02-12 13:53:36 -0300

    Adding the map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <button @click="logout">logout</button>
+  <div id="Home">
+
   </div>
+    <!-- <button @click="logout">logout</button> -->
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
-import { AUTH_LOGOUT } from '../store/actions/auth'
 
+import { AUTH_LOGOUT } from '../store/actions/auth'
+import gmapsInit from '../utils/gmaps'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    map: ''
+  }),
+  async mounted () {
+    try {
+      const google = await gmapsInit()
+      const geocoder = new google.maps.Geocoder()
+      const map = new google.maps.Map(this.$el, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+      })
+
+      geocoder.geocode({ address: 'Austria' }, (results, status) => {
+        if (status !== 'OK' || !results[0]) {
+          throw new Error(status)
+        }
+
+        map.setCenter(results[0].geometry.location)
+        map.fitBounds(results[0].geometry.viewport)
+      })
+    } catch (error) {
+      console.error(error)
+    }
   },
   methods: {
     logout: function () {
@@ -25,3 +45,14 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+  #Home {
+    height: 100vh;
+  }
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+</style>
