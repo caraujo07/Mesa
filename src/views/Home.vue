3cc8a5433997382b500c154071ba8488699aa54d
Cesar Oliveira dos Santos Araujo <caraujo@jc.com.br>
2020-02-13 17:34:48 -0300

    Getting user info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,39 @@
 <template>
   <div id="Home">
-    <div class="locationsList">asf</div>
+    <div class="locationsList">
+      <img :src="user.avatar" alt="">
+    </div>
     <google-map class="map" />
   </div>
 </template>
 <script>
+
 import GoogleMap from '../components/GoogleMap'
+import { api } from '../utils/api'
 
 export default {
   name: 'Home',
   components: {
     GoogleMap
+  },
+  data () {
+    return {
+      user: {
+        first_name: '',
+        email: '',
+        avatar: ''
+      }
+    }
+  },
+  async mounted () {
+    try {
+      const response = await api.get('/users/2')
+      this.user.first_name = response.data.data.first_name
+      this.user.email = response.data.data.email
+      this.user.avatar = response.data.data.avatar
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
 </script>
@@ -24,7 +47,7 @@ export default {
   }
   .locationsList {
     background: #FFF;
-    width: 20%;
+    width: 30%;
     height: 100vh;
   }
 </style>
